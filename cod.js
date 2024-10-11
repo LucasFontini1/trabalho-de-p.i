78d848577bb327f9d52f1b07cfffdabf0067a2bf
LucasFontini1 <lucasfontinidaluz@gmail.com>
2024-10-11 18:56:30 -0300

    finalização do formulario.

--- cod.js
+++ cod.js
@@ -1,26 +1,26 @@
 function vcJaEstaAqui(){
-    alert(`Você esta nessa pagina`)
-      homepage.innerHTML = (`Inicio`)
+    alert(`Você esta nessa pagina`);
+      homepage.innerHTML = (`Inicio`);
    }
    
    function vcJaEstaAquiDois(){
-   alert(`Você esta nessa pagina`)
-     homepage.innerHTML = (`Nossos Projetos`)
+   alert(`Você esta nessa pagina`);
+     homepage.innerHTML = (`Nossos Projetos`);
    }
    function vcJaEstaAquiTres(){
-     alert(`Você esta nessa pagina`)
-       homepage.innerHTML = (`Como Nos Ajudar`)
+     alert(`Você esta nessa pagina`);
+       homepage.innerHTML = (`Como Nos Ajudar`);
      }
      function vcJaEstaAquiQuatro(){
-       alert(`Você esta nessa pagina`)
-         homepage.innerHTML = (`Participantes`)
+       alert(`Você esta nessa pagina`);
+         homepage.innerHTML = (`Participantes`);
        }
    
      //PAG Como nos ajudar
-     const idade = document.getElementById('idade')
-       const nome = document.getElementById('nome')
-       const idadeValue = idade.value
-       const nomeValue = nome.value
+     const idade = document.getElementById('idade');
+       const nome = document.getElementById('nome');
+       const idadeValue = idade.value;
+       const nomeValue = nome.value;
    
    
    
@@ -30,20 +30,28 @@ function vcJaEstaAqui(){
    if (idade.value !== '' && nome.value !== '' ) {
    
        if(idade.value >= 18){
-         boasVindas.innerHTML = `ola ${nome.value}, obrigado por estar disposto a nos ajudar, para prosseguir acesse o link abaixo`
+         boasVindas.innerHTML = `ola ${nome.value}, obrigado por estar disposto a nos ajudar, para prosseguir acesse o link abaixo`;
          esseLink.innerHTML = `Clique Aqui`
    
          
        }else if(idade.value > 80){
-         boasVindas.innerHTML = `Infelizmente você não pode prosseguir tendo mais de 80 anos!`
+         boasVindas.innerHTML = `Infelizmente você não pode prosseguir tendo mais de 80 anos!`;
        }else{
-         boasVindas.innerHTML = `ola ${nome.value}, infelismente você não pode nos ajudar, sendo menor de idade`
+         boasVindas.innerHTML = `ola ${nome.value}, infelismente você não pode nos ajudar, sendo menor de idade`;
        }
      }else{
-       boasVindas.innerHTML = `Preencha os campos`
+       boasVindas.innerHTML = `Preencha os campos`;
      }
      }
      //  FORM DOIS
+     const email = document.getElementById = (`mail`);
+     const telefone = document.getElementById = (`telef`);
+     const cmAjudar = document.getElementById = ('cmAjudar');
+     const cidade = document.getElementById = ('cdd');
+
+     function send(){
+      
+     }
  
    
    
